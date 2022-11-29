8e775fb7b15e6049f94bb035c4841558b34cd878
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-11-29 08:15:42 +0200

    Added lab7

--- gfx-framework-master/src/lab_m1/lab7/shaders/VertexShader.glsl
+++ gfx-framework-master/src/lab_m1/lab7/shaders/VertexShader.glsl
@@ -22,18 +22,24 @@ uniform vec3 object_color;
 // Output value to fragment shader
 out vec3 color;
 
-
 void main()
 {
-    // TODO(student): Compute world space vectors
+    // Compute world space vectors
+    vec3 world_position = (Model * vec4(v_position, 1)).xyz;
+	vec3 world_normal	= normalize(mat3(Model) * normalize(v_normal)); // N
 
-    // TODO(student): Define ambient light component
+	vec3 V = normalize(eye_position - world_position);
+	vec3 L = normalize(light_position - world_position);
+	vec3 H = normalize(L + V);
+	vec3 R = normalize(reflect(L, world_normal));
+
+    // Define ambient light component
     float ambient_light = 0.25;
 
-    // TODO(student): Compute diffuse light component
-    float diffuse_light = 0;
+    // Compute diffuse light component
+    float diffuse_light = material_kd * ambient_light * max(dot(world_normal, L), 0);
 
-    // TODO(student): Compute specular light component
+    // Compute specular light component
     float specular_light = 0;
 
     // It's important to distinguish between "reflection model" and
@@ -41,15 +47,19 @@ void main()
     // (1975) and Blinn-Phong (1977) reflection models, and we are using the
     // Gouraud (1971) shading method. There is also the Phong (1975) shading
     // method, which we'll use in the future. Don't mix them up!
-    if (diffuse_light > 0)
+    if (diffuse_light > 0) // contine cos angle(N L)
     {
-
+        specular_light = material_ks * ambient_light * pow(max(dot(V, R), 0), material_shininess);
     }
 
-    // TODO(student): Compute light
+    // Compute light
+    float d = distance(light_position, v_position);
+    float attenuation_factor = 1 / (1 + 0.14 * d + 0.07 * d * d);
+    float light = ambient_light + attenuation_factor * (diffuse_light + specular_light);    // inmultim factorul de atenuare pentru intensitate
 
-    // TODO(student): Send color light output to fragment shader
-    color = vec3(1);
+
+    // Send color light output to fragment shader
+    color = object_color * light;
 
     gl_Position = Projection * View * Model * vec4(v_position, 1.0);
 }
