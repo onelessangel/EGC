16d83e1b9ee1439ec0d6edeff00aa26c639f1a3a
onelessangel <69592470+onelessangel@users.noreply.github.com>
2022-11-29 08:15:52 +0200

    Added lab7.cpp

--- gfx-framework-master/src/lab_m1/lab7/lab7.cpp
+++ gfx-framework-master/src/lab_m1/lab7/lab7.cpp
@@ -56,7 +56,7 @@ void Lab7::Init()
     // Light & material properties
     {
         lightPosition = glm::vec3(0, 1, 1);
-        materialShininess = 30;
+        materialShininess = 50;
         materialKd = 0.5;
         materialKs = 0.5;
     }
@@ -80,7 +80,7 @@ void Lab7::Update(float deltaTimeSeconds)
     {
         glm::mat4 modelMatrix = glm::mat4(1);
         modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 1, 0));
-        RenderSimpleMesh(meshes["sphere"], shaders["LabShader"], modelMatrix);
+        RenderSimpleMesh(meshes["sphere"], shaders["LabShader"], modelMatrix, glm::vec3(0, 0, 1));
     }
 
     {
@@ -88,14 +88,14 @@ void Lab7::Update(float deltaTimeSeconds)
         modelMatrix = glm::translate(modelMatrix, glm::vec3(2, 0.5f, 0));
         modelMatrix = glm::rotate(modelMatrix, RADIANS(60.0f), glm::vec3(1, 0, 0));
         modelMatrix = glm::scale(modelMatrix, glm::vec3(0.5f));
-        RenderSimpleMesh(meshes["box"], shaders["LabShader"], modelMatrix);
+        RenderSimpleMesh(meshes["box"], shaders["LabShader"], modelMatrix, glm::vec3(0, 1, 0));
     }
 
     {
         glm::mat4 modelMatrix = glm::mat4(1);
         modelMatrix = glm::translate(modelMatrix, glm::vec3(-2, 0.5f, 0));
         modelMatrix = glm::rotate(modelMatrix, RADIANS(60.0f), glm::vec3(1, 1, 0));
-        RenderSimpleMesh(meshes["box"], shaders["LabShader"], modelMatrix, glm::vec3(0, 0.5, 0));
+        RenderSimpleMesh(meshes["box"], shaders["LabShader"], modelMatrix, glm::vec3(1, 0, 0));
     }
 
     // Render ground
@@ -103,7 +103,7 @@ void Lab7::Update(float deltaTimeSeconds)
         glm::mat4 modelMatrix = glm::mat4(1);
         modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 0.01f, 0));
         modelMatrix = glm::scale(modelMatrix, glm::vec3(0.25f));
-        RenderSimpleMesh(meshes["plane"], shaders["LabShader"], modelMatrix);
+        RenderSimpleMesh(meshes["plane"], shaders["LabShader"], modelMatrix, glm::vec3(.7, 0, 0));
     }
 
     // Render the point light in the scene
@@ -131,12 +131,27 @@ void Lab7::RenderSimpleMesh(Mesh *mesh, Shader *shader, const glm::mat4 & modelM
     glUseProgram(shader->program);
 
     // Set shader uniforms for light & material properties
-    // TODO(student): Set light position uniform
+    // Set light position uniform
+    GLint loc_light_position = glGetUniformLocation(shader->program, "light_position");
+    glUniform3fv(loc_light_position, 1, glm::value_ptr(lightPosition));
 
+    // Set eye position (camera position) uniform
     glm::vec3 eyePosition = GetSceneCamera()->m_transform->GetWorldPosition();
-    // TODO(student): Set eye position (camera position) uniform
+    GLint loc_eye_position = glGetUniformLocation(shader->program, "eye_position");
+    glUniform3fv(loc_eye_position, 1, glm::value_ptr(eyePosition));
 
-    // TODO(student): Set material property uniforms (shininess, kd, ks, object color)
+    // Set material property uniforms (shininess, kd, ks, object color)
+    GLint loc_material_shininess = glGetUniformLocation(shader->program, "material_shininess");
+    glUniform1i(loc_material_shininess, materialShininess);
+
+    GLint loc_material_kd = glGetUniformLocation(shader->program, "material_kd");  // diffuse light
+    glUniform1f(loc_material_kd, materialKd);
+
+    GLint loc_material_ks = glGetUniformLocation(shader->program, "material_ks");  // specular light
+    glUniform1f(loc_material_ks, materialKs);
+
+    GLint loc_object = glGetUniformLocation(shader->program, "object_color");
+    glUniform3fv(loc_object, 1, glm::value_ptr(color));
 
     // Bind model matrix
     GLint loc_model_matrix = glGetUniformLocation(shader->program, "Model");
